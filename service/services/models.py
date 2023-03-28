f864f08c0a85d4f33d9ed6badcb7b4aa63b30e50
a-burlakov <fln@mail.ru>
2023-03-28 14:51:02 +0500

    feat: services models and migrations.

--- service/services/models.py
+++ service/services/models.py
@@ -1,3 +1,52 @@
+from django.core.validators import MaxValueValidator
 from django.db import models
 
-# Create your models here.
+from clients.models import Client
+
+
+class Service(models.Model):
+    """
+    Сервис.
+    """
+
+    name = models.CharField("Наименование", max_length=255)
+    full_price = models.PositiveIntegerField("Цена")
+
+
+class Plan(models.Model):
+    """
+    План подписки.
+    """
+
+    PLAN_TYPES = (
+        ("full", "Full"),
+        ("student", "Student"),
+        ("discount", "Discount"),
+    )
+
+    plan_type = models.CharField("Тип плана", choices=PLAN_TYPES, max_length=255)
+    discount_percent = models.PositiveIntegerField(
+        "Процент скидки",
+        default=0,
+        validators=[
+            MaxValueValidator(100),
+        ],
+    )
+
+
+class Subscription(models.Model):
+    """
+    Подписки клиентов на сервис.
+    """
+
+    client = models.ForeignKey(
+        Client,
+        verbose_name="Клиент",
+        related_name="subscriptions",
+        on_delete=models.PROTECT,
+    )
+    service = models.ForeignKey(
+        Service,
+        related_name="subscriptions",
+        on_delete=models.PROTECT,
+    )
