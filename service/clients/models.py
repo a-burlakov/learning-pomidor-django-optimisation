7f444e61a7c32b3bb37c9917107c2b24ec934ae6
a-burlakov <fln@mail.ru>
2023-03-28 14:42:13 +0500

    feat: client model and migrations

--- service/clients/models.py
+++ service/clients/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Client(models.Model):
+    user = models.OneToOneField(User, on_delete=models.PROTECT, verbose_name="Клиент")
+    company_name = models.CharField("Компания", max_length=255)
+    full_address = models.CharField("Адрес", max_length=1024)
