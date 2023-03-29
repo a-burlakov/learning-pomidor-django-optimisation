ea730b084b29abccac02820dc2d343f500f40a68
a-burlakov <fln@mail.ru>
2023-03-29 16:43:10 +0500

    feat: inner serializer.

--- service/services/serializers.py
+++ service/services/serializers.py
@@ -1,9 +1,16 @@
 from rest_framework import serializers
 
-from services.models import Subscription
+from services.models import Subscription, Plan
+
+
+class PlanSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Plan
+        fields = "__all__"
 
 
 class SubscriptionSerializer(serializers.ModelSerializer):
+    plan = PlanSerializer()
     client_name = serializers.CharField(source="client.company_name")
     email = serializers.CharField(source="client.user.email")
 
@@ -13,5 +20,5 @@ class SubscriptionSerializer(serializers.ModelSerializer):
             "id",
             "client_name",
             "email",
-            "plan_id",
+            "plan",
         )
