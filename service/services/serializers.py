5dc0ea51c96a915e83c87dad1dd4e958adc496c7
a-burlakov <fln@mail.ru>
2023-03-29 17:16:01 +0500

    lesson: aggregations and annotations.

--- service/services/serializers.py
+++ service/services/serializers.py
@@ -13,12 +13,11 @@ class SubscriptionSerializer(serializers.ModelSerializer):
     plan = PlanSerializer()
     client_name = serializers.CharField(source="client.company_name")
     email = serializers.CharField(source="client.user.email")
-    price = serializers.SerializerMethodField()
-
-    def get_price(self, instance):
-        return instance.service.full_price - (
-            instance.service.full_price * instance.plan.discount_percent / 100
-        )
+    price = serializers.FloatField()
+    # price = serializers.SerializerMethodField()
+    #
+    # def get_price(self, instance):
+    #     return instance.price
 
     class Meta:
         model = Subscription
