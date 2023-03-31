8d73a21a36c37c85a92b731fe5b32a11ec8240ee
a-burlakov <fln@mail.ru>
2023-03-31 15:44:48 +0500

    lesson: query optimisation.

--- service/services/tasks.py
+++ service/services/tasks.py
@@ -1,13 +1,19 @@
 from celery import shared_task
+from django.db.models import F
 
 from services.models import Subscription
 
 
 @shared_task
 def set_price(subscription_id):
-    subscription = Subscription.objects.get(id=subscription_id)
-    new_price = subscription.service.full_price - (
-        subscription.service.full_price * (subscription.plan.discount_percent / 100)
+    subscription = (
+        Subscription.objects.filter(id=subscription_id)
+        .annotate(
+            annotated_price=F("service__full_price")
+            - (F("service__full_price") * F("plan__discount_percent") / 100.00)
+        )
+        .first()
     )
-    subscription.price = new_price
+
+    subscription.price = subscription.annotated_price
     subscription.save()
--- service/services/views.py
+++ service/services/views.py
@@ -8,20 +8,14 @@ from services.serializers import SubscriptionSerializer
 
 
 class SubscriptionView(ReadOnlyModelViewSet):
-    queryset = (
-        Subscription.objects.all().prefetch_related(
-            "plan",
-            Prefetch(
-                "client",
-                queryset=Client.objects.all()
-                .select_related("user")
-                .only("company_name", "user__email"),
-            ),
-        )
-        # .annotate(
-        #     price=F("service__full_price")
-        #     - (F("service__full_price") * F("plan__discount_percent") / 100.00)
-        # )
+    queryset = Subscription.objects.all().prefetch_related(
+        "plan",
+        Prefetch(
+            "client",
+            queryset=Client.objects.all()
+            .select_related("user")
+            .only("company_name", "user__email"),
+        ),
     )
     serializer_class = SubscriptionSerializer
 
